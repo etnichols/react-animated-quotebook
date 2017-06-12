94ee8d6b03c960f39e4791c889619cf88128b729
Evan Nichols <evannichols@MacBook-Pro-28.attlocal.net>
2017-06-11 22:04:34 -0500

    basic shitty version working

--- src/App.js
+++ src/App.js
@@ -6,11 +6,36 @@ import './App.css';
 
 import quotes from './quotes'
 
-
 class App extends Component {
 
-  fetchRandomQuote(){
+  constructor(props){
+    super(props)
+    this.state = {
+      quote: '',
+      author: ''
+    }
+  }
 
+  getRandomQuote(){
+    console.log("getting new quote")
+    let min = Math.ceil(0)
+    let max = Math.floor(quotes.length)
+    let random_index = Math.floor(Math.random() * (max - min)) + min
+    this.setState({
+      quote: quotes[random_index]['quote'],
+      author: quotes[random_index]['author']
+    })
+  }
+
+  componentDidMount(){
+    let interval_id = setInterval(this.getRandomQuote.bind(this), 10000)
+    this.setState({
+      interval_id: interval_id
+    })
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.state.interval_id)
   }
 
   render() {
@@ -19,7 +44,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Quotebook</h2>
         </div>
-        <QuoteModal />
+        <QuoteModal quote={this.state.quote} author={this.state.author} />
       </div>
 
     );
--- src/components/QuoteModal.js
+++ src/components/QuoteModal.js
@@ -5,7 +5,7 @@ class QuoteModal extends Component {
   render(){
     return(
       <div className="text-center quote-modal">
-        <p>{this.props.quote}</p>
+        <p>"{this.props.quote}"</p>
           <hr/>
         <p>- {this.props.author}</p>
       </div>
