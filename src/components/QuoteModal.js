0ee060655154b77e576cb4e52658a86ba30ef654
Evan Nichols <evannichols@MacBook-Pro-28.attlocal.net>
2017-06-13 00:05:49 -0500

    fresh copy from dev, going with surge

---
+++ src/components/QuoteModal.js
@@ -0,0 +1,33 @@
+import { CSSTransitionGroup } from 'react-transition-group'
+import React, { Component } from 'react';
+import './styles.css'
+
+class QuoteModal extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    let quote = null
+    let author = null
+
+    if(this.props.quote !== ''){
+      quote = <p className="quote-text">"{this.props.quote}"</p>
+    }
+
+    if(this.props.author !== ''){
+      author = <p className="">- {this.props.author}</p>
+    }
+
+    return(
+      <div className="text-center quote-modal">
+        {quote}
+          <hr/>
+        {author}
+      </div>
+    )
+  }
+}
+
+export default QuoteModal
