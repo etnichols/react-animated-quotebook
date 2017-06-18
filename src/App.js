fe75c5ad332652a897ad86e8b61ded809f8ed988
Evan Nichols <evannichols@MacBook-Pro-28.attlocal.net>
2017-06-18 14:37:39 -0500

    new approach. way easier

--- src/App.js
+++ src/App.js
@@ -14,20 +14,38 @@ class App extends Component {
   constructor(props){
     super(props)
 
+    //randomize order of quotes at start of session
+    const shuffled = List(this.shuffleQuotes(quotes))
+    const init = shuffled.get(0)
+
     this.state = {
-      quote: '',
-      author: '',
-      background_color: '#668cc9',
-      session_history: List(),
-      available_quotes: List(quotes),
-      history_index: -1
+      quote: init.quote,
+      author: init.author,
+      available_quotes: shuffled,
+      index: 0
     }
   }
 
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffleQuotes(arr){
+    let curIndex = arr.length, tempVal, randIndex
+    while(0 !== curIndex){
+      //pick remaining element
+      randIndex = Math.floor(Math.random() * curIndex)
+      curIndex -= 1
+
+      //swap with current
+      tempVal = arr[curIndex]
+      arr[curIndex] = arr[randIndex]
+      arr[randIndex] = tempVal
+    }
+    return arr
+  }
+
   // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-  refreshBackgroundColor(){
+  generateRandomHexColor(){
     let hex_val = []
-    let possible = "0123456789ABCDEFG"
+    const possible = "0123456789ABCDEFG"
 
     for(let i = 0; i < 6; i++){
       hex_val.push(possible.charAt(Math.floor(Math.random() * possible.length)))
@@ -36,112 +54,35 @@ class App extends Component {
     hex_val.unshift('#')
     let color = hex_val.join('')
 
+    return color
+  }
+
+  performUpdate(){
+    const nq = this.state.available_quotes.get(this.state.index)
     this.setState({
-      background_color: color
+      quote: nq.quote,
+      author: nq.author,
+    }, () => {
+      console.log("performUpdate Completed")
     })
   }
 
-  unshiftSessionHistory(prevQuote){
-    const new_session_history = this.state.session_history.unshift(prevQuote)
-    this.setState({
-      session_history: new_session_history
-    })
-  }
-
-  getRandomQuote(){
-
-    const available_quotes = this.state.available_quotes
-
-    // update the session history with about-to-be-replaced quote
-    if(this.state.quote !== ''){
-        this.unshiftSessionHistory({
-          quote: this.state.quote,
-          author: this.state.author
-        })
-    }
-
-    let max = Math.floor(available_quotes.size)
-    let random_index = Math.floor(Math.random() * max)
-
-    //fetch and remove random quotes from available ones
-    let new_quote = available_quotes.get(random_index)
-    const new_available_quotes = available_quotes.delete(random_index)
-
-    this.setState({
-      quote: new_quote['quote'],
-      author: new_quote['author'],
-      available_quotes: new_available_quotes
-    })
-  }
-
-  /*
-    handleBackButton: fetch a past quote from the history, if it exists, using
-    the history_index state variable. Set the quote and author, and increase
-    the history_index by one.
-  */
   handleBackButton(){
-
-    const cur_history = this.state.session_history
-    const cur_index = this.state.history_index
-
-    console.log("handleBackButton(): curhistory: " + cur_history + " and cur_index: " + cur_index)
-
-    //two conditions we're guarding against here:
-      // 1. user tries to hit back button upon initial app load (i.e. no history no scroll through yet)
-      // 2. user has been scrolling back in history and we've hit the end.
-    if( !(cur_history.isEmpty()) && cur_index !== -1 ){
-
-      //preserve whatever quote we were on
-      if(this.state.quote !== ''){
-          this.unshiftSessionHistory({
-            quote: this.state.quote,
-            author: this.state.author
-          })
-      }
-
-      let prev = cur_history.get(cur_index)
-      this.setState({
-        quote: prev['quote'],
-        author: prev['author'],
-        history_index: cur_index + 1
-      })
-    }
+    this.setState({
+      index: this.state.index - 1
+    }, this.performUpdate)
   }
 
   handleNextButton(){
-
-    //we'll need to check and see if history index is 0 before fetching a new.
-    const cur_history = this.state.session_history
-    const cur_index = this.state.history_index
-
-    console.log("handleNextButton(): curhistory: " + cur_history + " and cur_index: " + cur_index)
-
-
-    if( cur_index === -1 ){
-      //then we'll fetch a new quote
-      this.getRandomQuote()
-      this.refreshBackgroundColor()
-
-    }
-    else{
-      //means user is making its way back through the history. So let's refresh using the next quote
-      let more_recent_quote = cur_history.get(cur_index)
-      this.setState({
-        quote: more_recent_quote['quote'],
-        author: more_recent_quote['author'],
-        history_index: cur_index - 1
-      })
-    }
-  }
-
-  componentWillMount(){
-    this.getRandomQuote()
+    this.setState({
+      index: this.state.index + 1
+    }, this.performUpdate)
   }
 
   render() {
-
+    const bgcolor = this.generateRandomHexColor()
     const container_style  = {
-      backgroundColor: this.state.background_color,
+      backgroundColor: bgcolor,
       transition: 'background-color 1s ease'
     }
 
